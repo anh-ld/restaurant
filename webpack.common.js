d1903b8d7603f9ef8eb4eedc4caea08894a0fa6d
Le Duy Anh <ledzanh@gmail.com>
2019-03-10 23:50:37 +0700

    :ambulance: Remove URL-loader

--- webpack.common.js
+++ webpack.common.js
@@ -37,17 +37,6 @@ module.exports = {
             outputPath: 'fonts/'
           }
         }]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/i,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              limit: 10000
-            }
-          }
-        ]
       }
     ]
   },
